17c7bd4eb6dfe2b8b91bc3bdf02692e02d37122f
positive666 <286040359@qq.com>
2021-09-26 13:47:45 +0800

    merge yolov5 author

--- models/yolo.py
+++ models/yolo.py
@@ -10,6 +10,7 @@ import sys
 from copy import deepcopy
 from pathlib import Path
 
+FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
