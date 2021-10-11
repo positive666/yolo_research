32c6d0b1811c6c2ea86d21e8d27f7f58c2e3fccb
positive666 <286040359@qq.com>
2021-10-11 12:01:38 +0800

    cancel ----default position_embedding  flag

--- models/common.py
+++ models/common.py
@@ -833,15 +833,17 @@ class CoordAtt(nn.Module):
         return out   
         
 class MHSA(nn.Module):
-    def __init__(self, n_dims, width=14, height=14, heads=4):
+    def __init__(self, n_dims, width=14, height=14, heads=4,pos_emb=False):
         super(MHSA, self).__init__()
 
         self.heads = heads
         self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
         self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
         self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.rel_h = nn.Parameter(torch.randn([1, heads, (n_dims ) // heads, 1, int(height)]), requires_grad=True)
-        self.rel_w = nn.Parameter(torch.randn([1, heads, (n_dims )// heads, int(width), 1]), requires_grad=True)
+        self.pos=pos_emb
+        if self.pos :
+            self.rel_h = nn.Parameter(torch.randn([1, heads, (n_dims ) // heads, 1, int(height)]), requires_grad=True)
+            self.rel_w = nn.Parameter(torch.randn([1, heads, (n_dims )// heads, int(width), 1]), requires_grad=True)
         self.softmax = nn.Softmax(dim=-1)
      
     def forward(self, x):
@@ -853,15 +855,18 @@ class MHSA(nn.Module):
         content_content = torch.matmul(q.permute(0,1,3,2), k) #1,C,h*w,h*w
         # print("qkT=",content_content.shape)
         c1,c2,c3,c4=content_content.size()
+        if self.pos:
        # print("old content_content shape",content_content.shape) #1,4,256,256
-        content_position = (self.rel_h + self.rel_w).view(1, self.heads, C // self.heads, -1).permute(0,1,3,2)   #1,4,1024,64
-       
-        content_position = torch.matmul(content_position, q)# ([1, 4, 1024, 256])
-        content_position=content_position if(content_content.shape==content_position.shape)else content_position[:,: , :c3,]
-        assert(content_content.shape==content_position.shape)
+            content_position = (self.rel_h + self.rel_w).view(1, self.heads, C // self.heads, -1).permute(0,1,3,2)   #1,4,1024,64
+           
+            content_position = torch.matmul(content_position, q)# ([1, 4, 1024, 256])
+            content_position=content_position if(content_content.shape==content_position.shape)else content_position[:,: , :c3,]
+            assert(content_content.shape==content_position.shape)
         #print('new pos222-> shape:',content_position.shape)
        # print('new content222-> shape:',content_content.shape)
-        energy = content_content + content_position
+            energy = content_content + content_position
+        else:
+            energy=content_content
         attention = self.softmax(energy)
         out = torch.matmul(v, attention.permute(0,1,3,2)) #1,4,256,64
         out = out.view(n_batch, C, width, height)
