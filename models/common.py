1cdd8ab730269b45dd99b9f91fb5e0389a75db3c
positive666 <286040359@qq.com>
2021-12-27 18:15:26 +0800

    add GAM3D -attention

--- models/common.py
+++ models/common.py
@@ -1100,4 +1100,36 @@ class CTR3(nn.Module):
     def forward(self, x):
         #print("CTR3-INPUT:",x.shape)
        # return self.cv3
-        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))                  
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))  
+        
+class GAM_Attention(nn.Module):
+   #https://paperswithcode.com/paper/global-attention-mechanism-retain-information
+    def __init__(self, in_channels, out_channels, rate=4):
+        super(GAM_Attention, self).__init__()
+
+        self.channel_attention = nn.Sequential(
+            nn.Linear(in_channels, int(in_channels / rate)),
+            nn.ReLU(inplace=True),
+            nn.Linear(int(in_channels / rate), in_channels)
+        )
+
+        self.spatial_attention = nn.Sequential(
+            nn.Conv2d(in_channels, int(in_channels / rate), kernel_size=7, padding=3),
+            nn.BatchNorm2d(int(in_channels / rate)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(int(in_channels / rate), out_channels, kernel_size=7, padding=3),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x_permute = x.permute(0, 2, 3, 1).view(b, -1, c)
+        x_att_permute = self.channel_attention(x_permute).view(b, h, w, c)
+        x_channel_att = x_att_permute.permute(0, 3, 1, 2)
+
+        x = x * x_channel_att
+
+        x_spatial_att = self.spatial_attention(x).sigmoid()
+        out = x * x_spatial_att
+
+        return out        
