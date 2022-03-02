126d066001ffb06d6be73b36a410472d5638f47d
positive666 <286040359@qq.com>
2022-03-02 11:06:47 +0800

    fix decoupled tensor index

--- models/yolo.py
+++ models/yolo.py
@@ -114,8 +114,7 @@ class Decoupled_Detect(nn.Module):
             x[i] = self.m[i](x[i])  # conv  
           
             reg_heads=self.reg_c2(self.reg_c1(x[i]))  #回归头
-            x[i]=torch.cat([self.cls_head(self.cls_c2(self.cls_c1(x[i]))),self.reg_head(reg_heads),self.obj_head(reg_heads)],1)
-          
+            x[i]=torch.cat([self.reg_head(reg_heads),self.obj_head(reg_heads),self.cls_head(self.cls_c2(self.cls_c1(x[i])))],1)
                   
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
