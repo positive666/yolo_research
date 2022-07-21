16ab2b763b5c6b6bee9a9341734e230295d5bec7
positive666 <286040359@qq.com>
2022-07-21 19:46:20 +0800

    fix

--- train.py
+++ train.py
@@ -299,6 +299,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
         # dataset.mosaic_border = [b - imgsz, -b]  # height, width borders
 
         mloss = torch.zeros(3, device=device)  # mean losses
+        if ota_match or aux_ota_loss:
+            mloss=torch.zeros(4, device=device)  # mean losses
         if RANK != -1:
             train_loader.sampler.set_epoch(epoch)
         pbar = enumerate(train_loader)
