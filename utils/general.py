afb0fb2d4aa7f3e6a47a26edc951361bda667d00
positive666 <286040359@qq.com>
2022-09-15 20:33:13 +0800

    fix

--- utils/general.py
+++ utils/general.py
@@ -839,8 +839,8 @@ def clip_coords(boxes, shape,step=None):
     # Clip bounding xyxy bounding boxes to image shape (height, width)
     
     if step is not  None:
-        boxes[:, 0::step].clamp_(0, img_shape[1])  # x1
-        boxes[:, 1::step].clamp_(0, img_shape[0])  # y1
+        boxes[:, 0::step].clamp_(0, shape[1])  # x1
+        boxes[:, 1::step].clamp_(0, shape[0])  # y1
 
     elif isinstance(boxes, torch.Tensor):  # faster individually
         boxes[:, 0].clamp_(0, shape[1])  # x1
