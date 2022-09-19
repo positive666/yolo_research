55a8412e3a70ee3cb8493f10cc4649339219ee4e
positive666 <286040359@qq.com>
2022-09-19 12:23:33 +0800

    fix nms

--- utils/general.py
+++ utils/general.py
@@ -868,8 +868,7 @@ def non_max_suppression(prediction,
         prediction = prediction[0]  # select only inference output
         
     bs = prediction.shape[0]  # batch size
-    if nc is None:
-        nc = prediction.shape[2] -nm- 5  if not kpt_label else prediction.shape[2] - 56 # number of classes
+    nc = prediction.shape[2] -nm- 5  if not kpt_label else prediction.shape[2] - 56 # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
 
     # Checks
