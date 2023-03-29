c751ab7cf92d165b8b4f19d56d0572726aa05f85
cheng <286040359@qq.com>
2023-03-29 07:01:09 -0300

    fix v8 core

--- utils/tal.py
+++ utils/tal.py
@@ -220,9 +220,9 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
     return torch.cat(anchor_points), torch.cat(stride_tensor)
 
 
-def dist2bbox(distance, anchor_points, xywh=True, dim=-1): '''distance:为Dfl输出的结果，代表anchors中心点的距离值，根据距离值转换BOX坐标'''
+def dist2bbox(distance, anchor_points, xywh=True, dim=-1): 
     """Transform distance(ltrb) to box(xywh or xyxy)."""
-    lt, rb = torch.split(distance, 2, dim)     '''分别取出lt,rb的距离对值'''
+    lt, rb = torch.split(distance, 2, dim)     
     x1y1 = anchor_points - lt                  #计算坐标
     x2y2 = anchor_points + rb
     if xywh:                                 ##bOX 格式转换
