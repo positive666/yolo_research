98c869c183a2441a4b0d1102b187da9e79a8f9c2
cheng <286040359@qq.com>
2023-04-28 00:46:09 -0300

    add auto label tools

--- models/experimental.py
+++ models/experimental.py
@@ -137,7 +137,7 @@ class Ensemble(nn.ModuleList):
         y = [module(x, augment, profile, visualize)[0] for module in self]
         # y = torch.stack(y).max(0)[0]  # max ensemble
         # y = torch.stack(y).mean(0)  # mean ensemble
-        y = torch.cat(y, 1)  # nms ensemble
+        y = torch.cat(y, 2)  # nms ensemble
         return y, None  # inference, train output
 
 def torch_safe_load(weight):
