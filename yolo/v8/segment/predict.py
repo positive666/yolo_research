fb6c1709d9d128e6494e34ff967a08a08b5dc81b
cheng <286040359@qq.com>
2023-05-04 03:21:08 -0300

    fix

--- yolo/v8/segment/predict.py
+++ yolo/v8/segment/predict.py
@@ -11,7 +11,7 @@ from yolo.v8.detect.predict import DetectionPredictor
 
 
 class SegmentationPredictor(DetectionPredictor):
-     def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
+    def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
         super().__init__(cfg, overrides, _callbacks)
         self.args.task = 'segment'
         
